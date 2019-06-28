e9951fe229b4e9093339ee09c0279d22486b033d
filippsorin <tothemoon>
2019-06-28 19:17:01 +0300

    some improvements v3

--- src/collecting/builders/ParameterBuilder.ts
+++ src/collecting/builders/ParameterBuilder.ts
@@ -1,13 +1,14 @@
-import {SchemaBuilder} from "../SchemaBuilder";
-import {ParameterDeclaration} from "ts-morph";
+import {ParameterDeclaration, TypeGuards} from "ts-morph";
 import {ParameterSchema} from "../../schema/declarations/ParameterSchema";
 import {Qualifier} from "../../common/qualifier/Qualifier";
 import {BuildManager} from "../BuildManager";
 import {Class} from "../../common/utils/Class";
 import {Builder} from "../../common/utils/Builder";
+import { Node } from "ts-morph";
 import {TypeSchema} from "../../schema/type/TypeSchema";
+import {GenericNodeBuilder} from "./GenericNodeBuilder";
 
-export class ParameterBuilder extends SchemaBuilder<ParameterDeclaration, ParameterSchema> {
+export class ParameterBuilder extends GenericNodeBuilder<ParameterDeclaration, ParameterSchema> {
 	public build(q: Qualifier, n: ParameterDeclaration, manager: BuildManager): ParameterSchema {
 		return Builder.of(ParameterSchema).setAndBuild({
 			qualifier: q,
@@ -18,15 +19,11 @@ export class ParameterBuilder extends SchemaBuilder<ParameterDeclaration, Parame
 	}
 
 	public match(n: unknown, manager: BuildManager): n is ParameterDeclaration {
-		return undefined;
-	}
-
-	public qualifier(n: ParameterDeclaration): Qualifier {
-		return undefined;
+		return n instanceof Node && TypeGuards.isParameterDeclaration(n);
 	}
 
 	public schema(): Class<ParameterSchema> {
-		return undefined;
+		return ParameterSchema;
 	}
 
 }
